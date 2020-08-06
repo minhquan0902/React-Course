265483cf8b6f72681d376bcb89d82b43ba94415d
minhquan0902 <minhquan0902@gmail.com>
2020-08-07 03:24:00 +0800

    Route practice

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-
+import Home from "./HomeComponent";
 import Menu from './MenuComponent';
 import Header from "./HeaderComponent";
 import {DISHES} from '../shared/dishes';
+import { Switch, Route, Redirect} from 'react-router-dom';
 import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent';
 
@@ -12,19 +13,24 @@ class Main extends Component {
 
     this.state ={
       dishes: DISHES,
-      selectedDish: null
     };
   }
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-}
+
   render(){
+
+  const HomePage =()=>{
+    return(
+      <Home/>
+    );
+  }
   return (
     <div className="App">
       <Header></Header>
-      <Menu dishes ={this.state.dishes}
-            onClick={(dishId) => this.onDishSelect(dishId)}/>
-      <DishDetail dish ={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]} />
+      <Switch>
+        <Route path ="/home" component={HomePage}/>
+        <Route exact path="/menu" component={() =><Menu dishes ={this.state.dishes}/>}/>
+        <Redirect to ="/home"/>
+      </Switch>
       <Footer/>
     </div>
   );
