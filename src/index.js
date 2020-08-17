a0b18b0c9d210822c70a8559736afbce9bf7c887
minhquan0902 <minhquan0902@gmail.com>
2020-08-17 16:55:55 +0800

    Redux update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import 'font-awesome/css/font-awesome.min.css';
